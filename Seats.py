c8566b7563bfc1a9e850f14f0d3c682b518b0e94
ggisreal <73349645+ggisreal@users.noreply.github.com>
2022-06-29 19:25:52 +0800

    Update Seats.py

--- Seats.py
+++ Seats.py
@@ -9,10 +9,10 @@ Class Solution:
             return 0
         MOD = 10000003
         ans = float('inf')
-        for start in range(len(A)):
-            total = 0
-            for cross in crosses:
-                total += abs(cross - start)
-                total %= MOD
-            ans = min(ans, total %= MOD)
+        start = crosses[n // 2]
+        total = 0
+        for cross in crosses:
+            total += abs(cross - start)
+            total %= MOD
+        ans = min(ans, total %= MOD)
         return ans
