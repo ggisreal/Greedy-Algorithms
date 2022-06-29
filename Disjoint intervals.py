3ed43591d6ff669bd9eaf950b49e20afd199bb95
ggisreal <73349645+ggisreal@users.noreply.github.com>
2022-06-29 15:53:32 +0800

    Create Disjoint intervals.py

---
+++ Disjoint intervals.py
@@ -0,0 +1,18 @@
+Class Solution:
+    # @Param A: list of lists of integers
+    # @return an integer
+    
+    def solve(self, A):
+        A.sort(key = lambda x: x[1])
+        
+        prev_s, prev_e = A[0]
+        count = 1
+        
+        for s, e in A:
+            if s <= prev_e:
+                pass
+            else:
+                prev_s, prev_e = s, e
+                count += 1
+                
+        return count
