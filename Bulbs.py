ee3e3ede88d230b8ac554bc9f216b802bdcc033d
ggisreal <73349645+ggisreal@users.noreply.github.com>
2022-06-29 14:06:41 +0800

    Update Bulbs.py

--- Bulbs.py
+++ Bulbs.py
@@ -1,1 +1,15 @@
-Class Solution
+Class Solution:
+    # @Param A: List of integers
+    # @return an integer
+    
+    def bulb(self, A):
+        cost = 0
+        
+        for b in A:
+            if cost % 2 == 0: b = b
+            else: b = not b
+                
+            if b % 2 == 1: continue
+            else: cost += 1
+        
+        return cost
