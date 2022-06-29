ba8110a6448592f8d0fe38681d6b8956fff0aa2b
ggisreal <73349645+ggisreal@users.noreply.github.com>
2022-06-29 19:18:38 +0800

    Update Seats.py

--- Seats.py
+++ Seats.py
@@ -7,4 +7,12 @@ Class Solution:
         n = len(crosses)
         if n == 0:
             return 0
-            
+        MOD = 10000003
+        ans = float('inf')
+        for start in range(len(A)):
+            total = 0
+            for cross in crosses:
+                total += abs(cross - start)
+                total %= MOD
+            ans = min(ans, total %= MOD)
+        return ans
